ca230bedb3028d5e4d692e50b627715e3c79c061
yegor256 <yegor256@gmail.com>
2020-06-17 09:10:48 +0300

    #66 show method name

--- lib/sibit/bestof.rb
+++ lib/sibit/bestof.rb
@@ -39,28 +39,28 @@ class Sibit
 
     # Current price of BTC in USD (float returned).
     def price(currency = 'USD')
-      best_of do |api|
+      best_of('price') do |api|
         api.price(currency)
       end
     end
 
     # Gets the balance of the address, in satoshi.
     def balance(address)
-      best_of do |api|
+      best_of('balance') do |api|
         api.balance(address)
       end
     end
 
     # Get the height of the block.
     def height(hash)
-      best_of do |api|
+      best_of('height') do |api|
         api.height(hash)
       end
     end
 
     # Get the hash of the next block.
     def next_of(hash)
-      best_of do |api|
+      best_of('next_of') do |api|
         api.next_of(hash)
       end
     end
@@ -68,49 +68,49 @@ class Sibit
     # Get recommended fees, in satoshi per byte. The method returns
     # a hash: { S: 12, M: 45, L: 100, XL: 200 }
     def fees
-      best_of(&:fees)
+      best_of('fees', &:fees)
     end
 
     # Fetch all unspent outputs per address.
     def utxos(keys)
-      best_of do |api|
+      best_of('utxos') do |api|
         api.utxos(keys)
       end
     end
 
     # Latest block hash.
     def latest
-      best_of(&:latest)
+      best_of('latest', &:latest)
     end
 
     # Push this transaction (in hex format) to the network.
     def push(hex)
-      best_of do |api|
+      best_of('push') do |api|
         api.push(hex)
       end
     end
 
     # This method should fetch a block and return as a hash.
     def block(hash)
-      best_of do |api|
+      best_of('block') do |api|
         api.block(hash)
       end
     end
 
     private
 
-    def best_of
+    def best_of(method)
       return yield @list unless @list.is_a?(Array)
       results = []
       @list.each do |api|
         begin
           results << yield(api)
         rescue Sibit::Error => e
-          @log.info("The API #{api.class.name} failed: #{e.message}")
+          @log.info("The API #{api.class.name} failed at #{method}(): #{e.message}")
         end
       end
       if results.empty?
-        raise Sibit::Error, "No APIs out of #{@api.length} managed to succeed: \
+        raise Sibit::Error, "No APIs out of #{@api.length} managed to succeed at #{method}(): \
 #{@api.map { |a| a.class.name }.join(', ')}"
       end
       results.group_by(&:to_s).values.max_by(&:size)[0]
--- lib/sibit/firstof.rb
+++ lib/sibit/firstof.rb
@@ -39,28 +39,28 @@ class Sibit
 
     # Current price of BTC in USD (float returned).
     def price(currency = 'USD')
-      first_of do |api|
+      first_of('price') do |api|
         api.price(currency)
       end
     end
 
     # Gets the balance of the address, in satoshi.
     def balance(address)
-      first_of do |api|
+      first_of('balance') do |api|
         api.balance(address)
       end
     end
 
     # Get the height of the block.
     def height(hash)
-      first_of do |api|
+      first_of('height') do |api|
         api.height(hash)
       end
     end
 
     # Get the hash of the next block.
     def next_of(hash)
-      first_of do |api|
+      first_of('next_of') do |api|
         api.next_of(hash)
       end
     end
@@ -68,38 +68,38 @@ class Sibit
     # Get recommended fees, in satoshi per byte. The method returns
     # a hash: { S: 12, M: 45, L: 100, XL: 200 }
     def fees
-      first_of(&:fees)
+      first_of('fees', &:fees)
     end
 
     # Fetch all unspent outputs per address.
     def utxos(keys)
-      first_of do |api|
+      first_of('utxos') do |api|
         api.utxos(keys)
       end
     end
 
     # Latest block hash.
     def latest
-      first_of(&:latest)
+      first_of('latest', &:latest)
     end
 
     # Push this transaction (in hex format) to the network.
     def push(hex)
-      first_of do |api|
+      first_of('push') do |api|
         api.push(hex)
       end
     end
 
     # This method should fetch a block and return as a hash.
     def block(hash)
-      first_of do |api|
+      first_of('block') do |api|
         api.block(hash)
       end
     end
 
     private
 
-    def first_of
+    def first_of(method)
       return yield @list unless @list.is_a?(Array)
       done = false
       result = nil
@@ -109,11 +109,11 @@ class Sibit
           done = true
           break
         rescue Sibit::Error => e
-          @log.info("The API #{api.class.name} failed: #{e.message}")
+          @log.info("The API #{api.class.name} failed at #{method}(): #{e.message}")
         end
       end
       unless done
-        raise Sibit::Error, "No APIs out of #{@api.length} managed to succeed: \
+        raise Sibit::Error, "No APIs out of #{@api.length} managed to succeed at #{method}(): \
 #{@api.map { |a| a.class.name }.join(', ')}"
       end
       result
