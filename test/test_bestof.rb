004f3dc58d532c9d5a1bea354774d073de9f762b
Yegor Bugayenko <yegor256@gmail.com>
2024-01-06 19:34:20 +0300

    year up

--- test/test_bestof.rb
+++ test/test_bestof.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright (c) 2019-2023 Yegor Bugayenko
+# Copyright (c) 2019-2024 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -27,7 +27,7 @@ require_relative '../lib/sibit/bestof'
 
 # Sibit::BestOf test.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2019-2023 Yegor Bugayenko
+# Copyright:: Copyright (c) 2019-2024 Yegor Bugayenko
 # License:: MIT
 class TestBestOf < Minitest::Test
   def test_not_array
