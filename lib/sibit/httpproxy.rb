6a0d85ccb8656e2b4a85a4df21229837a37ac9f7
Yegor Bugayenko <yegor256@gmail.com>
2025-12-27 08:33:19 +0300

    #173: Add HttpProxy class for handling HTTP requests with proxy.

--- lib/sibit/httpproxy.rb
+++ lib/sibit/httpproxy.rb
@@ -7,14 +7,18 @@ require 'net/http'
 
 # Sibit main class.
 class Sibit
-  # HTTP interface.
+  # HTTP client with proxy.
   #
   # Author:: Yegor Bugayenko (yegor256@gmail.com)
   # Copyright:: Copyright (c) 2019-2025 Yegor Bugayenko
   # License:: MIT
-  class Http
+  class HttpProxy
+    def initialize(addr)
+      @host, @port = addr.split(':')
+    end
+
     def client(uri)
-      http = Net::HTTP.new(uri.host, uri.port)
+      http = Net::HTTP.new(uri.host, uri.port, @host, @port.to_i)
       http.use_ssl = true
       http.read_timeout = 240
       http
---
+++ test/test_http.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# SPDX-FileCopyrightText: Copyright (c) 2019-2025 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
+require_relative 'test__helper'
+require_relative '../lib/sibit/http'
+
+# Sibit::Http test.
+# Author:: Yegor Bugayenko (yegor256@gmail.com)
+# Copyright:: Copyright (c) 2019-2025 Yegor Bugayenko
+# License:: MIT
+class TestHttp < Minitest::Test
+  def test_creates_http_client
+    http = Sibit::Http.new
+    uri = URI.parse('https://example.com/path')
+    client = http.client(uri)
+    assert_kind_of(Net::HTTP, client, 'client is not Net::HTTP')
+  end
+
+  def test_enables_ssl
+    http = Sibit::Http.new
+    uri = URI.parse('https://example.com/path')
+    client = http.client(uri)
+    assert_predicate(client, :use_ssl?, 'SSL is not enabled')
+  end
+
+  def test_sets_read_timeout
+    http = Sibit::Http.new
+    uri = URI.parse('https://example.com/path')
+    client = http.client(uri)
+    assert_equal(240, client.read_timeout, 'read timeout is not 240')
+  end
+end
